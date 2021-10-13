03789ef05d1fe10ca01e6669b9c8ddda0fba54f5
WeimarC <>
2021-10-13 16:46:16 -0300

    A

---
+++ src/components/Header.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+import "./Header.css";
+const header = () => {
+  return (
+    <header>
+      <div id="cajaBuscador">
+        <input type="text" name="buscar" id="buscar" placeholder="Buscar" />
+        <img
+          id="lupa"
+          src="./iconoBuscar.svg"
+          alt=""
+          width="32px"
+          height="32px"
+        />
+      </div>
+    </header>
+  );
+};
+
+export default header;
